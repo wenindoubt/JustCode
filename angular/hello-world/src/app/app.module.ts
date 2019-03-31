6ac3bccde86809616ed67b11d71a6381ac7f0638
Jeffrey Wen <jeffrey@wenindoubt.com>
2019-03-30 18:31:07 -0700

    Class, Style, & Event Bindings

--- angular/hello-world/src/app/app.module.ts
+++ angular/hello-world/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { JwenTestComponent } from './jwen-test/jwen-test.component';
@@ -10,7 +11,8 @@ import { JwenTestComponent } from './jwen-test/jwen-test.component';
     JwenTestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
