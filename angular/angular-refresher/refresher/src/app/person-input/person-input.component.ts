4c46a29baa31b0d3296f0e2e45a8651a5540c37a
Jeffrey Wen <jeffrey@wenindoubt.com>
2019-05-27 14:48:23 -0700

    Retrieve Data via ngModel

--- angular/angular-refresher/refresher/src/app/person-input/person-input.component.ts
+++ angular/angular-refresher/refresher/src/app/person-input/person-input.component.ts
@@ -7,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonInputComponent implements OnInit {
 
-  onCreatePerson(personName: string) {
-    console.log('Created a person: ' + personName);
+  enteredPersonName = '';
+
+  onCreatePerson() {
+    console.log('Created a person: ' + this.enteredPersonName);
+    this.enteredPersonName = '';
   }
 
   constructor() { }
