562297f25344a45a0ccd9650a486d9a4dcf3bc3a
Jeffrey Wen <jeffrey@wenindoubt.com>
2019-05-27 14:26:56 -0700

    AppComponent -> CustomComponent communication
    - ngFor directive to display properties

--- angular/angular-refresher/refresher/src/app/persons/persons.component.ts
+++ angular/angular-refresher/refresher/src/app/persons/persons.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-persons',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./persons.component.css']
 })
 export class PersonsComponent implements OnInit {
+  @Input() personList: string[];
 
   constructor() { }
 
