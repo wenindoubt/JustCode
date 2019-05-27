c7348d1bfe7973bb2c0067eeb7b72c74ee120bc6
Jeffrey Wen <jeffrey@wenindoubt.com>
2019-05-27 14:41:20 -0700

    Local Reference

--- angular/angular-refresher/refresher/src/app/person-input/person-input.component.ts
+++ angular/angular-refresher/refresher/src/app/person-input/person-input.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonInputComponent implements OnInit {
 
+  onCreatePerson(personName: string) {
+    console.log('Created a person: ' + personName);
+  }
+
   constructor() { }
 
   ngOnInit() {
