ded7f2ad87a4121cfd344733e7126f1dd6e98b5b
Jeffrey Wen <jeffrey@wenindoubt.com>
2019-05-27 14:35:19 -0700

    Generate person-input Component

---
+++ angular/angular-refresher/refresher/src/app/person-input/person-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-person-input',
+  templateUrl: './person-input.component.html',
+  styleUrls: ['./person-input.component.css']
+})
+export class PersonInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
