7ea7cd221b987637fa08fdec74c1f7f74a4b63a4
Jeffrey Wen <jeffrey@wenindoubt.com>
2019-05-27 16:18:44 -0700

    Push Data with Subjects/Subscriptions

--- angular/angular-refresher/refresher/src/app/persons/persons.component.ts
+++ angular/angular-refresher/refresher/src/app/persons/persons.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { PersonsService } from './persons.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html',
   styleUrls: ['./persons.component.css']
 })
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit, OnDestroy {
   personList: string[];
+  private personListSub: Subscription;
 
   constructor(private personsService: PersonsService) {
 
@@ -15,10 +17,17 @@ export class PersonsComponent implements OnInit {
 
   ngOnInit() {
     this.personList = this.personsService.persons;
+    this.personListSub = this.personsService.personsChanged.subscribe(persons => {
+      this.personList = persons;
+    });
   }
 
   onRemovePerson(personName: string) {
     this.personsService.removePerson(personName);
+  }
+
+  ngOnDestroy() {
+    this.personListSub.unsubscribe();
   }
 
 }
--- angular/angular-refresher/refresher/src/app/persons/persons.service.ts
+++ angular/angular-refresher/refresher/src/app/persons/persons.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonsService {
+  personsChanged = new Subject<string[]>();
   persons: string[] = ['Jack', 'Anna', 'George'];
 
   addPerson(name: string) {
     this.persons.push(name);
+    this.personsChanged.next(this.persons);
   }
 
   removePerson(name: string) {
     this.persons = this.persons.filter(person => {
       return person !== name;
     });
+    this.personsChanged.next(this.persons);
     console.log(this.persons);
   }
 
